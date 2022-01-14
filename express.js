c8a7b39542460d54163e31b06d9925a0715e50c7
Rodolfo Rezende <rodolfo.rezende@vetta.digital>
2022-01-14 10:33:59 -0300

    worked with custom view

--- express.js
+++ express.js
@@ -10,6 +10,28 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+const { urlencoded } = require('express'); // eslint-disable-line import/no-unresolved
+
+const body = urlencoded({ extended: false });
+
+const { strict: assert } = require('assert');
+const querystring = require('querystring');
+const { inspect } = require('util');
+
+const isEmpty = require('lodash/isEmpty');
+
+
+const keys = new Set();
+const debug = (obj) => querystring.stringify(Object.entries(obj).reduce((acc, [key, value]) => {
+  keys.add(key);
+  if (isEmpty(value)) return acc;
+  acc[key] = inspect(value, { depth: null });
+  return acc;
+}, {}), '<br/>', ': ', {
+  encodeURIComponent(value) { return keys.has(value) ? `<strong>${value}</strong>` : value; },
+});
+
 const configuration = require('./support/configuration');
 
 configuration.findAccount = async (ctx, id) => {
@@ -22,83 +44,167 @@ configuration.findAccount = async (ctx, id) => {
   }
 }
 
-const oidc = new Provider('http://localhost:3000',  { adapter, ...configuration} );
+const provider = new Provider('http://localhost:3000',  { adapter, ...configuration} );
 
-app.use("/oidc",oidc.callback());
+app.use("/oidc", provider.callback());
 
-app.get('/view/:viewName', (req, res) => {
-  res.render(req.params.viewName, { uid: req.query.jti });
-});
+const { constructor: { errors: { SessionNotFound } } } = provider;
 
-app.get('/interaction/:uid', async (req, res, next) => {
-  const details = await oidc.interactionDetails(req, res);
-  if (details.prompt.name === 'consent'){
-    return await conceiveGrant(req, res);
-  }
-  return await authenticate(req, res);
-});
-
-const authenticate = async (req, res) => {
-  const result = {
-    // authentication/login prompt got resolved, omit if no authentication happened, i.e. the user
-    // cancelled
-    login: {
-      accountId: 'rodolfo', // logged-in account id
-      acr: 'string', // acr value for the authentication
-      remember: true, // true if provider should use a persistent cookie rather than a session one, defaults to true
-      ts: now(), // unix timestamp of the authentication, defaults to now()
-    }
-  }
-  await oidc.interactionFinished(req, res, result);
-}
-
-const conceiveGrant = async (req, res) => {
-  const {
-    prompt: { name, details }, grantId, session, params, missingOIDCScope, missingOIDCClaims, missingResourceScopes
-  } = await oidc.interactionDetails(req, res);
-  let grant;
-  if (grantId) {
-    // we'll be modifying existing grant in existing session
-    grant = await oidc.Grant.find(grantId);
-  } else {
-    // we're establishing a new grant
-    grant = new oidc.Grant({
-      accountId: session.accountId,
-      clientId: params.client_id,
+app.use((req, res, next) => {
+  const orig = res.render;
+  // you'll probably want to use a full blown render engine capable of layouts
+  res.render = (view, locals) => {
+    app.render(view, locals, (err, html) => {
+      if (err) throw err;
+      orig.call(res, '_layout', {
+        ...locals,
+        body: html,
+      });
     });
-  }
+  };
+  next();
+});
 
-  if (missingOIDCScope) {
-    grant.addOIDCScope(missingOIDCScope.join(' '));
-  }
-  if (missingOIDCClaims) {
-    grant.addOIDCClaims(missingOIDCClaims);
-  }
-  if (missingResourceScopes) {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const [indicator, scope] of Object.entries(missingResourceScopes)) {
-      grant.addResourceScope(indicator, scope.join(' '));
-    }
-  }
-
-  const result = { consent: { grantId: await grant.save() } };
-  await oidc.interactionFinished(req, res, result, {mergeWithLastSubmission: true});
+function setNoCache(req, res, next) {
+  res.set('Pragma', 'no-cache');
+  res.set('Cache-Control', 'no-cache, no-store');
+  next();
 }
 
+app.get('/interaction/:uid', setNoCache, async (req, res, next) => {
+  try {
+    const {
+      uid, prompt, params, session,
+    } = await provider.interactionDetails(req, res);
 
-app.post('/interaction/:uid/login', async (req, res) => {
-  const result = {
-    // authentication/login prompt got resolved, omit if no authentication happened, i.e. the user
-    // cancelled
-    login: {
-      accountId: '7ff1d19a-d3fd-4863-978e-8cce75fa880c', // logged-in account id
-      acr: 'string', // acr value for the authentication
-      remember: true, // true if provider should use a persistent cookie rather than a session one, defaults to true
-      ts: now(), // unix timestamp of the authentication, defaults to now()
+    const client = await provider.Client.find(params.client_id);
+
+    switch (prompt.name) {
+      case 'login': {
+        return res.render('login', {
+          client,
+          uid,
+          details: prompt.details,
+          params,
+          title: 'Sign-in',
+          session: session ? debug(session) : undefined,
+          dbg: {
+            params: debug(params),
+            prompt: debug(prompt),
+          },
+        });
+      }
+      case 'consent': {
+        return res.render('interaction', {
+          client,
+          uid,
+          details: prompt.details,
+          params,
+          title: 'Authorize',
+          session: session ? debug(session) : undefined,
+          dbg: {
+            params: debug(params),
+            prompt: debug(prompt),
+          },
+        });
+      }
+      default:
+        return undefined;
     }
+  } catch (err) {
+    return next(err);
   }
-  await oidc.interactionFinished(req, res, result);
-  await next();
+});
+
+app.post('/interaction/:uid/login', setNoCache, body, async (req, res, next) => {
+  try {
+    const { prompt: { name } } = await provider.interactionDetails(req, res);
+    assert.equal(name, 'login');
+    
+    //const account = await Account.findByLogin(req.body.login);
+
+    const result = {
+      // authentication/login prompt got resolved, omit if no authentication happened, i.e. the user
+      // cancelled
+      login: {
+        accountId: 'rodolfo', // logged-in account id
+        acr: 'string', // acr value for the authentication
+        remember: true, // true if provider should use a persistent cookie rather than a session one, defaults to true
+        ts: now(), // unix timestamp of the authentication, defaults to now()
+      }
+    }
+
+    await provider.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/interaction/:uid/confirm', setNoCache, body, async (req, res, next) => {
+  try {
+    const interactionDetails = await provider.interactionDetails(req, res);
+    const { prompt: { name, details }, params, session: { accountId } } = interactionDetails;
+    assert.equal(name, 'consent');
+
+    let { grantId } = interactionDetails;
+    let grant;
+
+    if (grantId) {
+      // we'll be modifying existing grant in existing session
+      grant = await provider.Grant.find(grantId);
+    } else {
+      // we're establishing a new grant
+      grant = new provider.Grant({
+        accountId,
+        clientId: params.client_id,
+      });
+    }
+
+    if (details.missingOIDCScope) {
+      grant.addOIDCScope(details.missingOIDCScope.join(' '));
+    }
+    if (details.missingOIDCClaims) {
+      grant.addOIDCClaims(details.missingOIDCClaims);
+    }
+    if (details.missingResourceScopes) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [indicator, scopes] of Object.entries(details.missingResourceScopes)) {
+        grant.addResourceScope(indicator, scopes.join(' '));
+      }
+    }
+
+    grantId = await grant.save();
+
+    const consent = {};
+    if (!interactionDetails.grantId) {
+      // we don't have to pass grantId to consent, we're just modifying existing one
+      consent.grantId = grantId;
+    }
+
+    const result = { consent };
+    await provider.interactionFinished(req, res, result, { mergeWithLastSubmission: true });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/interaction/:uid/abort', setNoCache, async (req, res, next) => {
+  try {
+    const result = {
+      error: 'access_denied',
+      error_description: 'End-User aborted interaction',
+    };
+    await provider.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof SessionNotFound) {
+    // handle interaction expired / session not found error
+  }
+  next(err);
 });
 
 app.listen(3000, function () {
